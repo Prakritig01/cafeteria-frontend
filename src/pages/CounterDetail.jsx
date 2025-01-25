7407b1623336b6f9517df81f5f2bd68a9f466182
Prakritig01 <prakritigupta1112002@gmail.com>
2025-01-25 11:11:35 +0530

    app setup

---
+++ src/pages/CounterDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CounterDetail = () => {
+  return (
+    <div>
+      Counter Detail Page.
+    </div>
+  )
+}
+
+export default CounterDetail
---
+++ src/pages/MerchantPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MerchantPage = () => {
+  return (
+    <div>
+      Merchant Page
+    </div>
+  )
+}
+
+export default MerchantPage
